9c04d67bf1a983d032e953c0cf7d542eeb1e1ed7
Victor King'e <vinge1718@gmail.com>
2017-08-21 07:08:39 +0300

    Add the ability to recognise empty inputs and zero inputs

--- JS/script.js
+++ JS/script.js
@@ -3,23 +3,26 @@ $(document).ready(function(){
     var firstNumber = parseInt($("#firstNumber").val());
     var secondNumber = parseInt($("#secondNumber").val());
     var thirdNumber = parseInt($("#thirdNumber").val());
-    var equilateral = "form an equilateral traingle";
-    var isosceles = "form an Isosceles triangle";
-    var scalene = "form a Scalene triangle";
-    var noTriangle = "not be enough to form a triangle";
-    var naught = "not do much else";
+    var equilateral = "The measurements will form <br>An equilateral traingle";
+    var isosceles = "The measurements will form <br> An Isosceles triangle";
+    var scalene = "The measurements will form <br> A Scalene triangle";
+    var noTriangle = "The measurements will not be enough to form a triangle";
+    var naught = "The measurements will not do much else";
 
-
-    if(firstNumber === secondNumber && secondNumber === thirdNumber){
-      $("p#output").empty().append(equilateral);
-    }else if (firstNumber === secondNumber || firstNumber === thirdNumber || secondNumber === thirdNumber) {
-      $("p#output").empty().append(isosceles);
-    }else if (firstNumber !== secondNumber && secondNumber!== thirdNumber || firstNumber !==thirdNumber) {
-      $("p#output").empty().append(scalene);
-    }else if ((firstNumber+secondNumber <= thirdNumber) || (secondNumber+thirdNumber <= firstNumber)) {
-      $("p#output").empty().append(noTriangle);
-    } else {
-      $("p#output").empty().append(naught);
+  if (isNaN(firstNumber)||isNaN(secondNumber)||isNaN(thirdNumber)){
+    alert("Enter a number bigger than zero in all fields!");
+  }else if ((firstNumber=== 0) || (secondNumber === 0) || (thirdNumber=== 0)) {
+      alert("Please enter a number bigger than Zero")
+    } else if((firstNumber === secondNumber) && (secondNumber === thirdNumber)){
+        $("p#output").empty().append(equilateral);
+      }else if ((firstNumber === secondNumber) || (firstNumber === thirdNumber) || (secondNumber === thirdNumber)) {
+        $("p#output").empty().append(isosceles);
+      }else if ((firstNumber !== secondNumber) && (secondNumber!== thirdNumber) || (firstNumber !==thirdNumber)) {
+        $("p#output").empty().append(scalene);
+      }else if ((firstNumber+secondNumber <= thirdNumber) || (secondNumber+thirdNumber <= firstNumber)) {
+        $("p#output").empty().append(noTriangle);
+      } else {
+        $("p#output").empty().append(naught);
     }
 
     $("p#output").show();
